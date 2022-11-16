bb2a0a5c2b59ff21fb84a58940aa0ceb66842d8c
ilialecha <ilia.lecha@alum.esci.upf.edu>
2022-11-16 16:20:36 +0100

    master

--- Lab-7/permutations.py
+++ Lab-7/permutations.py
@@ -1,18 +1,16 @@
 import sys
 def permutations(s):
     S = list(s)
-
     L = {""}
-
     while len(S) != 0:
         x = S.pop()
         LL = set()
         for w in L:
             for i in range(len(L)+1):
-                LL.add(w[:i]+x+w[i:])
+                LL.add(w[:i] + x + w[i:])
         L = LL
     return sorted(L)
 
-s = sys.stdin.readline().split()
+s = sys.stdin.readline().strip()
 
 for w in permutations(s): print(w)
